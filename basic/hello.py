bc03a62af5f5b2a1fc0617520cd2a4db30fb7231
JahirPendhari <jahirpp1999@gmail.com>
2024-05-08 13:46:28 +0530

    first Program using python

--- basic/hello.py
+++ basic/hello.py
@@ -1,2 +1,46 @@
-print("hello World")
-print("Welcome to Learning Python")
+# print("hello World")
+# print("Welcome to Learning Python")
+# name = "jahir"
+# age = 25
+# is_adult = True
+
+# taking input from user
+# firstname = input("What is your firstname? ")
+# lastname = input("What is your firstname? ")
+# print("Hello "+firstname +" "+lastname)
+
+# how to convert string to number (integer)
+
+# old_age = input("old Age? ")
+# new_age = int(old_age)+2
+# print("new age :",new_age)
+
+# numb = 18
+# print(float(numb))  ans 18.0
+
+# add to numbers
+
+# first = input("enter you first number? ")
+# second = input("enter your second number? ")
+# sum = int(first) + int(second)
+# print("Sum of Numbers :-", str(sum))
+
+
+# name = "tony stark"
+
+# print('s' in name)  ans :- True
+# print('b' in name)  ans :- False
+
+# print(3>2 or 3<2)   ans - true
+# print(3>2 and 3<2)  ans - false
+
+
+# in python we don't have block {} we use indentation
+
+# age = 18
+# if age > 18:
+#     print("you are adult")
+# elif age==18 :
+#     print("now you are adult")
+# else:
+#     print("you are not adult")
